5a918e6047f9dc40a1be6a7a915322a2542767c3
Dmitrii <boruno93@gmail.com>
2024-03-13 18:50:42 +0200

    Movement fixed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,21 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    
-    private bool isMoving;
-    
+
     private Vector2 input;
 
     private void Update()
     {
-        if (!isMoving)
-        {
-            input.x = Input.GetAxisRaw("Horizontal");
-            input.y = Input.GetAxisRaw("Vertical");
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
 
-            if (input != Vector2.zero)
-            {
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-                
-                StartCoroutine(Move(targetPos));
-            }
-        }
+        var targetPos = transform.position;
+        targetPos.x += input.x * speed * Time.deltaTime;
+        targetPos.y += input.y * speed * Time.deltaTime;
+
+        transform.position = targetPos;
     }
-    
-    private IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-        
-        while ((targetPos - transform.position).sqrMagnitude > float.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            yield return null;
-        }
-        
-        isMoving = false;
-    }
-    
-    
 }
