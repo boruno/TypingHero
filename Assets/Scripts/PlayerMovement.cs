96ba0c7973f64cae598c9740f11df9ef28090179
Dmitrii <boruno93@gmail.com>
2024-04-03 19:17:39 +0300

    Fixes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,33 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 4.5f;
     public Rigidbody2D rb;
+    public Camera mainCamera;
+    public Camera typingCamera;
     Vector2 input;
     Vector2 movement;
 
-    private void Update()
+    private void FixedUpdate()
     {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
 
-        movement = input * speed * Time.deltaTime;
+        movement = Vector2.Lerp(movement, input * speed, Time.deltaTime * 10f);
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        movement = Vector2.zero; // Reset the movement after applying it
     }
 
-    private void FixedUpdate()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        rb.MovePosition(rb.position + movement);
-        movement = Vector2.zero; // Reset the movement after applying it
+        if (collision.gameObject.tag == "Teleporter")
+        {
+            // Disable the current camera
+            mainCamera.enabled = false;
+
+            // Enable the "Typing Camera"
+            typingCamera.enabled = true;
+
+            // Disable player movement
+            speed = 0;
+        }
     }
 }
